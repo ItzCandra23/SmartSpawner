48a6a0472feeb23ca9b909735a9504ff5f23617f
ItzCandra23 <itzcandra23@gmail.com>
2025-11-13 01:11:52 +0700

    UPDATE

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,15 +1,24 @@
 import { system, world } from "@minecraft/server";
 import { configuration, SmartSpawner } from "./smartspawner";
-system.runInterval(() => {
-    for (const spawner of SmartSpawner.getAllSmartSpawner()) {
-        const location = spawner.location;
-        const dimensionId = spawner.dimensionId;
-        const dimension = world.getDimension(dimensionId);
-        if (!dimension.isChunkLoaded(location))
-            continue;
-        const players = dimension.getPlayers({ location, maxDistance: configuration.spawner.range });
-        if (!players.length)
-            return;
-        system.runJob(SmartSpawner.generateSpawnerLoot(location, dimensionId));
-    }
-}, configuration.spawner.delay * 20);
+let interval = generateInterval(configuration.spawner.delay);
+function generateInterval(seconds) {
+    return system.runInterval(() => {
+        for (const spawner of SmartSpawner.getAllSmartSpawner()) {
+            const location = spawner.location;
+            const dimensionId = spawner.dimensionId;
+            const dimension = world.getDimension(dimensionId);
+            if (!dimension.isChunkLoaded(location))
+                continue;
+            const players = dimension.getPlayers({ location, maxDistance: configuration.spawner.range });
+            if (!players.length)
+                return;
+            system.runJob(SmartSpawner.generateSpawnerLoot(location, dimensionId));
+        }
+    }, seconds * 20);
+}
+export function updateInterval(seconds) {
+    if (!seconds)
+        seconds = configuration.spawner.delay;
+    system.clearRun(interval);
+    interval = generateInterval(seconds);
+}
--- src/generator.ts
+++ src/generator.ts
@@ -1,17 +1,28 @@
 import { system, world } from "@minecraft/server";
 import { configuration, SmartSpawner } from "./smartspawner";
 
-system.runInterval(() => {
-    for (const spawner of SmartSpawner.getAllSmartSpawner()) {
-        const location = spawner.location;
-        const dimensionId = spawner.dimensionId;
-        const dimension = world.getDimension(dimensionId);
+let interval = generateInterval(configuration.spawner.delay);
 
-        if (!dimension.isChunkLoaded(location)) continue;
+function generateInterval(seconds: number): number {
+    return system.runInterval(() => {
+        for (const spawner of SmartSpawner.getAllSmartSpawner()) {
+            const location = spawner.location;
+            const dimensionId = spawner.dimensionId;
+            const dimension = world.getDimension(dimensionId);
 
-        const players = dimension.getPlayers({ location, maxDistance: configuration.spawner.range });
-        if (!players.length) return;
+            if (!dimension.isChunkLoaded(location)) continue;
 
-        system.runJob(SmartSpawner.generateSpawnerLoot(location, dimensionId));
-    }
-}, configuration.spawner.delay * 20);
+            const players = dimension.getPlayers({ location, maxDistance: configuration.spawner.range });
+            if (!players.length) return;
+
+            system.runJob(SmartSpawner.generateSpawnerLoot(location, dimensionId));
+        }
+    }, seconds * 20);
+}
+
+export function updateInterval(seconds?: number) {
+    if (!seconds) seconds = configuration.spawner.delay;
+
+    system.clearRun(interval);
+    interval = generateInterval(seconds);
+}
